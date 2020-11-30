faa765b71a46b70b63d89ec74d6ceeb7b900cbe0
davicavalcante <74988139+davaicavalcante@users.noreply.github.com>
2020-11-30 14:06:13 -0300

    Meu primeiro programa

--- Projeto 01 Inicio/MeuPrograma.java
+++ Projeto 01 Inicio/MeuPrograma.java
@@ -1,5 +1,5 @@
 class MeuPrograma {
     public static void main(String[] args){
-        System.out.println("Minha primeira aplicaçao em java")
+        System.out.println("Minha primeira aplicaçao em java!")
     }
 }
